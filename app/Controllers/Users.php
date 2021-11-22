890ceed7287895dcd00a23e830b2a69176635540
riyanris <riyanrisky129@gmail.com>
2021-11-22 20:05:29 +0700

    anjing babi bangsat

--- app/Controllers/Users.php
+++ app/Controllers/Users.php
@@ -118,13 +118,13 @@ class Users extends BaseController
 				}
 
 				$lastid = $this->users->simpan($additionalData);
-				
+
 				$settingNotif = [
 					'user' => $lastid,
 					'login' => 0
 				];
 				$this->setting_notif->simpan($settingNotif);
-				
+
 				if ($lastid) {
 					$rep = $this->log("insert", $lastid, "users");
 					$this->report_to_admin("add_user", $rep);
