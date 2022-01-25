c72cb3d5356e4f80e687fe50789fffe8e230735d
riyanris <riyanrisky129@gmail.com>
2022-01-25 15:23:03 +0700

    halo syangku

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -315,31 +315,25 @@ class Home extends BaseController
 		return $res;	
 	}
 
-	function tes(){
-
+	function uji(){
 		$time_start1 = microtime(true);
 		$users = $this->users->findAll();
 		$bot_token = env("BOT_TOKEN_TELE");
 		$bot = new \Telegram($bot_token);
-		$msg = "Halo.."; $no = 1; $sukses = 0;
+		$msg = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 		$users = $this->users->findAll();
 		foreach($users as $key){
 			if($key['chat_id'] == null) continue;
 			$content = ['chat_id' => $key['chat_id'], 'text' => $msg, 'parse_mode' => 'HTML'];
 			$time_start = microtime(true);
-			$statistik = $bot->sendMessage($content);
-			if($statistik['ok'] == 1){
-				$sukses++;
-			}
+			$bot->sendMessage($content);
 			$time_end = microtime(true);
 			$time = $time_end - $time_start;
-			echo "[".time()."]\nPesan ke-".$no++." kepada ".$key['nama']." dalam $time seconds.\n";
-			tampilkan($statistik);
-			echo "\n\n\n";
+			echo$time;
 		}
 		$time_end1 = microtime(true);
 		$time1 = $time_end1 - $time_start1;
-		echo "Semua pesan dikirim dalam $time1 detik. Kepada $no orang.";
+		echo "Semua pesan dikirim dalam $time1 detik.";
 	}
 
 }
